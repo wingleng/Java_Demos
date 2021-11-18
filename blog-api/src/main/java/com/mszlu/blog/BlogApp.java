defff4482be6c5b2d29f862f795c4c0ae3326c55
wongGNOW <923084107@qq.com>
2021-11-18 15:13:22 +0800

    添加了图片上传的功能，并且加入了对配置文件中的密码加密的功能。

--- blog-api/src/main/java/com/mszlu/blog/BlogApp.java
+++ blog-api/src/main/java/com/mszlu/blog/BlogApp.java
@@ -1,11 +1,12 @@
 package com.mszlu.blog;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties//该配置是用来加密配置文件中的账号密码的。
 public class BlogApp {
-
     public static void main(String[] args) {
         SpringApplication.run(BlogApp.class,args);
     }
--- blog-api/src/test/java/JustTest.java
+++ blog-api/src/test/java/JustTest.java
@@ -1,12 +1,29 @@
 import org.apache.commons.codec.digest.DigestUtils;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class JustTest {
+
+    StringEncryptor stringEncryptor;
+
     @Test
     public void test(){
         String salt = "mszlu!@#";
         String password = DigestUtils.md5Hex(1 + salt);
 
         System.out.println(password);
+     }
+     @Test
+    public void getPassword(){
+         BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+         //加密所需的salt(盐)
+         textEncryptor.setPassword("");
+         //要加密的数据（数据库的用户名或密码）
+         String username = textEncryptor.encrypt("");
+         String password = textEncryptor.encrypt("");
+         System.out.println("username:\n"+username);
+         System.out.println("password:\n"+password);
      }
 }
