c93ef7e5f8691821ad202efe9508d7802d677ebb
wongGNOW <923084107@qq.com>
2021-11-18 21:36:18 +0800

    终章，完成了文章归档显示功能。 项目总体上是完成了

--- blog-api/src/main/java/com/mszlu/blog/dao/mapper/ArticleMapper.java
+++ blog-api/src/main/java/com/mszlu/blog/dao/mapper/ArticleMapper.java
@@ -1,6 +1,8 @@
 package com.mszlu.blog.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.mszlu.blog.dao.dos.Archives;
 import com.mszlu.blog.dao.pojo.Article;
 
@@ -8,4 +10,10 @@ import java.util.List;
 
 public interface ArticleMapper extends BaseMapper<Article> {
     List<Archives> listArchives();
+
+    IPage<Article> listArticle(Page<Article>page,
+                               Long categoryId,
+                               Long tagId,
+                               String year,
+                               String month);
 }
