a6dc11a24be35669804c17cd9d1fbac8e20446fd
wongGNOW <923084107@qq.com>
2021-12-26 09:59:07 +0800

    最终部署上线版，之前部署失败的原因居然是因为prod的配置文件没有生效（因为dockerfile那里spring.profiles.active那里写少了一个s，导致环境没有触发。。。。

--- blog-api/src/main/java/com/mszlu/blog/config/WebMVCConfig.java
+++ blog-api/src/main/java/com/mszlu/blog/config/WebMVCConfig.java
@@ -11,12 +11,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMVCConfig implements WebMvcConfigurer {
     @Autowired
     private LoginInterceptor loginInterceptor;
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        //跨域配置，不可设置为*，不安全, 前后端分离项目，可能域名不一致
+//        //本地测试 端口不一致 也算跨域
+//        registry.addMapping("/**").allowedOrigins("http://www.wingloong.top");
+//    }
+
+    /**
+     * 另外一个跨域配置：
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        //跨域配置，不可设置为*，不安全, 前后端分离项目，可能域名不一致
-        //本地测试 端口不一致 也算跨域
-        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET","HEAD","POST","PUT","DELETE","OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
     }
+
     /**
      * 配置一个登录拦截器
      */
